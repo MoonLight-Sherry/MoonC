18441b0dcae539be1656411aa57941c0ab058abd
MoonLight-Sherry <3397446353@qq.com>
2021-07-31 13:42:03 +0800

    Jira Id: fix sort bugs in OutputExtreme.

--- cLanguageLearn/OutputExtreme/OutputExtreme.cpp
+++ cLanguageLearn/OutputExtreme/OutputExtreme.cpp
@@ -1,22 +1,23 @@
 ﻿#include <stdio.h>
 #include "sort.h"
-#define MAX 11
-int *pa[MAX];
+
 
 
 int main()
 {
-	int pa[10];
-	printf("输入待排序数字");
+	const int MAX = 11;
+	int pa[MAX];
+
+	printf("输入待排序数字input");
 	for (int i = 0; i <= 9; i++)
 	{
 		scanf_s("%d", &pa[i]);
 	}
 
-	int *left = pa[0];
-	int *right = pa[9];
+	int * left = &pa[0];
+	int * right = pa + 9;
 
 
-	sort(pa[10], left, right);
+	sort(pa, left, right);
 	printf("max = %d, min = %d\n", pa[0], pa[9]);
 }
--- cLanguageLearn/OutputExtreme/sort.cpp
+++ cLanguageLearn/OutputExtreme/sort.cpp
@@ -10,8 +10,8 @@ void sort(int *pa, int *left, int *right)
 	last = left;
 	for (i = left+1 ; i <= right; i++)
 		if ((*i) < (*left))
-			swap(&pa[(*++last)], &pa[(*i)]);
-	swap(&pa[(*left)], &pa[*last]);
+			swap(++last, i);
+	swap(left, last);
 	sort(pa,left, last - 1);
 	sort(pa, last + 1, right);
 }
