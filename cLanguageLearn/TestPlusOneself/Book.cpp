2fc71427ceddfb8c6117dfaf56fb839fbba8a713
MoonLight-Sherry <3397446353@qq.com>
2021-08-07 12:29:22 +0800

    Jira Id: fix bugs in class.

--- cLanguageLearn/TestPlusOneself/Book.cpp
+++ cLanguageLearn/TestPlusOneself/Book.cpp
@@ -1,20 +1,5 @@
 #include "Book.h"
 
-class BookTest
-{
-public :
-	explicit BookTest(char a);//explicit限制类型转换
-	void setTitle(char a) { Title = a; }
-	void serPrice(int price) { this->price = price; }
-	int getPrice() const { return price; }
-	BookTest(BookTest &b);//默认只拷贝指针
-	char Title;
-	int price;
-	~ BookTest();//析构函数
-private:
-	BookTest() {};
-	static int count;
-};
 
 int BookTest::count = 0;//static定义必须在函数外
 
@@ -34,4 +19,5 @@ int BookTest::count = 0;//static定义必须在函数外
 	{
 		const BookTest Alice('I');
 		const char * Test = "test";
+		return 0;
 	}
--- cLanguageLearn/TestPlusOneself/initialClassLearn.cpp
+++ cLanguageLearn/TestPlusOneself/initialClassLearn.cpp
@@ -1,20 +1,8 @@
 #include "initialClassLearn.h"
 #include <iostream>
+#include "Box.h"
+
 using namespace std;
-
-class Box
-{
-	Box(int l);
-
-private:
-	Box * TOPointer;
-	int length;
-
-private:
-	inline void set_length(int l = 1);//inline强制定义为内联函数//设置默认参数需要在参数列表结尾
-	int get_length() { return length; }
-
-};
 
 Box MilkBox;
 Box &Milk = MilkBox;
